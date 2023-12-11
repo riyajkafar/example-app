68e2c7d32f839f9e60fea1d3015ddc959e00ea7e
Ahla Fayis <139907384+FathimaAhla@users.noreply.github.com>
2023-12-11 09:51:56 +0530

    feat: add authentication to shops and group routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ShopController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,6 +27,17 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::prefix('shops')->group(function () {
+        Route::get('/shops', [ShopController::class, 'index'])->name('shops.index');
+        Route::get('/shops/create', [ShopController::class, 'create'])->name('shops.create');
+        Route::post('/shops/store', [ShopController::class, 'store'])->name('shops.store');
+        Route::get('/shops/{id}/edit', [ShopController::class, 'edit'])->name('shops.edit');
+        Route::put('/shops/{shop}', [ShopController::class, 'update'])->name('shops.update');
+        Route::delete('/shops/{shop}', [ShopController::class, 'destroy'])->name('shops.destroy');
+    });
 });
 
 require __DIR__.'/auth.php';
+
+
