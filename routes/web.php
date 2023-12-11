96f73eaeab9cf6a3c55c472d2191a4f468332244
mrmrifad <rifadm32@gmail.com>
2023-12-11 10:51:28 +0530

    feat: add authentication to accounts and group routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AccountController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,6 +27,17 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::prefix('accounts')->group(function () {
+        Route::get('/', [AccountController::class, 'index'])->name('accounts.index');
+        Route::get('/create', [AccountController::class, 'create'])->name('accounts.create');
+        Route::post('/', [AccountController::class, 'store'])->name('accounts.store');
+        Route::get('/{account}/edit', [AccountController::class, 'edit'])->name('accounts.edit');
+        Route::put('/{account}', [AccountController::class, 'update'])->name('accounts.update');
+        Route::delete('/{account}', [AccountController::class, 'destroy'])->name('accounts.destroy');
+    });
 });
 
 require __DIR__.'/auth.php';
+
+
