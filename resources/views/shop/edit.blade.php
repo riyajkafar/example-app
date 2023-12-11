5ecbd3ce6e0065b52683b2c989f5ff6f22e4538f
Ahla Fayis <139907384+FathimaAhla@users.noreply.github.com>
2023-12-11 09:46:35 +0530

    feat: create Shop UI and add attribute in Index, Create, and Edit views

---
+++ resources/views/shop/edit.blade.php
@@ -0,0 +1,71 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Edit Shop') }}
+        </h2>
+    </x-slot>
+
+    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+            <form method="POST" action="{{ route('shops.update', $shop->id) }}">
+                @csrf
+                @method('PUT')
+
+                <!-- Name -->
+                <div>
+                    <x-input-label for="name" :value="__('Shop Name')" />
+                    <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name', $shop->name)"
+                        required autofocus autocomplete="name" />
+                    <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                </div>
+
+                <!-- Website -->
+                <div>
+                    <x-input-label for="website" :value="__('Website')" />
+                    <x-text-input id="website" class="block mt-1 w-full" type="text" name="website"
+                        :value="old('website', $shop->website)" required autofocus autocomplete="website" />
+                    <x-input-error :messages="$errors->get('website')" class="mt-2" />
+                </div>
+
+                <!-- Contact No -->
+                <div>
+                    <x-input-label for="contact" :value="__('Contact No')" />
+                    <x-text-input id="contact" class="block mt-1 w-full" type="text" name="contact"
+                        :value="old('contact', $shop->contact)" required autofocus autocomplete="contact" />
+                    <x-input-error :messages="$errors->get('contact')" class="mt-2" />
+                </div>
+
+                <!-- Address -->
+                <div>
+                    <x-input-label for="address" :value="__('Shop Address')" />
+                    <x-text-input id="address" class="block mt-1 w-full" type="text" name="address"
+                        :value="old('address', $shop->address)" required autofocus autocomplete="address" />
+                    <x-input-error :messages="$errors->get('address')" class="mt-2" />
+                </div>
+
+                <!-- Description -->
+                <div>
+                    <x-input-label for="description" :value="__('Description')" />
+                    <x-form-textarea id="description" name="description" rows="4" :value="old('description')" required
+                        class="w-full" />
+                    <x-input-error :messages="$errors->get('description')" class="mt-2" />
+
+                </div>
+
+                <div class="flex items-center justify-end mt-4">
+                    <x-primary-button class="ms-4">
+                        {{ __('Update Shop') }}
+                    </x-primary-button>
+                </div>
+
+                <div class="text-center">
+                    <x-nav-link href="{{ route('shops.index') }}">
+                        Back to Shop List
+                    </x-nav-link>
+
+                </div>
+            </form>
+        </div>
+
+    </div>
+</x-app-layout>
---
+++ resources/views/shop/index.blade.php
@@ -0,0 +1,91 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Shop') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-6">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <div class="flex items-center justify-end mt-1 mb-2">
+                        <x-secondary-button class="ms-4">
+                            <a href="{{ route('shops.create') }}">
+                            {{ __('Add New Shop') }}
+                            </a>
+                        </x-secondary-button>
+                    </div>
+                    <table class="items-center bg-transparent w-full border-collapse ">
+                        <thead>
+                            <tr>
+                                <th
+                                    class="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                                    Shop Name
+                                </th>
+                                <th
+                                    class="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                                    Website
+                                </th>
+                                <th
+                                    class="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                                    Address
+                                </th>
+                                <th
+                                    class="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                                    Contact No
+                                </th>
+                                <th
+                                    class="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                                    Description
+                                </th>
+                                <th
+                                    class="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                                    Action
+                                </th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            @foreach ($shops as $key => $shop)
+                                <tr>
+                                    <td
+                                        class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700">
+                                        {{ $shop->name }}
+                                    </td>
+                                    <td
+                                        class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 ">
+                                        {{ $shop->website }}
+                                    </td>
+                                    <td
+                                        class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                                        {{ $shop->contact }}
+                                    </td>
+                                    <td
+                                        class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                                        {{ $shop->address }}
+                                    </td>
+                                    <td
+                                        class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                                        {{ $shop->description }}
+                                    </td>
+                                    <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                                        <a href="{{route('shops.edit', $shop->id)}}" class="bg-green-100 text-green-800 active:bg-green-100 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 dark:text-green-400 border border-green-400" type="button">Edit</a>
+
+                                        <form action="{{route('shops.destroy', $shop->id)}}" method="POST">
+                                        @method('DELETE')
+                                        @csrf
+                                        <button type="submit" class="bg-red-100 text-red-800 active:bg-red-200 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 dark:text-red-400 border border-red-400">Delete</button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            @endforeach
+
+                        </tbody>
+
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
