6263949d50a7f5f43607c8bdb7a3fe8d2b6a721a
Rameez <121714501+rameezmuhammadh@users.noreply.github.com>
2023-12-11 09:53:01 +0530

    feat: add authentication to payments and group routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\ProfileController;
+use App\Models\Payment;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,6 +28,21 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Payment routes 
+Route::prefix('payments')->group(function(){
+    Route::get('/', [PaymentController::class,'index'])->name('payments.index');
+    Route::get('/create', [PaymentController::class, 'create'])->name('payments.create');
+    Route::post('/store', [PaymentController::class,'store'])->name('payments.store');
+    Route::get('/show/{payment}',  [PaymentController::class,'show'])->name('payments.show');
+    Route::get('/{id}/edit', [PaymentController::class,'edit'])->name('payments.edit');
+    Route::put('/{payment}',  [PaymentController::class,'update'])->name('payments.update');
+    Route::delete('/{payment}',  [PaymentController::class,'destroy'])->name('payments.destroy');
+    });
+    
 });
 
 require __DIR__.'/auth.php';
+
+
+
