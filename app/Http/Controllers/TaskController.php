cb58ef98c7f3d5b8d5c86d6c5a199b1bc395966e
mezzims <rahman.ms706@gmail.com>
2023-12-11 09:31:21 +0530

    fix: update validation in TaskController

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,43 +2,58 @@
 
 namespace App\Http\Controllers;
 
-
 use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
     public function index()
-{
-    $tasks = Task::all();
-    return view('tasks.index', compact('tasks'));
-}
+    {
+        $tasks = Task::all();
+        return view('tasks.index', compact('tasks'));
+    }
 
-public function create()
-{
-    return view('tasks.create');
-}
+    public function create()
+    {
+        return view('tasks.create');
+    }
 
-public function store(Request $request)
-{
-    Task::create($request->all());
-    return redirect()->route('tasks.index');
-}
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'added_date' => 'required|date',
+            'deadline_date' => 'required|date',
+            'status' => 'required|string',
+        ]);
 
-public function edit(Task $task)
-{
-    return view('tasks.edit', compact('task'));
-}
+        Task::create($request->all());
+        return redirect()->route('tasks.index');
+    }
 
-public function update(Request $request, Task $task)
-{
-    $task->update($request->all());
-    return redirect()->route('tasks.index');
-}
+    public function edit(Task $task)
+    {
+        return view('tasks.edit', compact('task'));
+    }
 
-public function destroy(Task $task)
-{
-    $task->delete();
-    return redirect()->route('tasks.index');
-}
+    public function update(Request $request, Task $task)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'added_date' => 'required|date',
+            'deadline_date' => 'required|date',
+            'status' => 'required|string',
+        ]);
+
+        $task->update($request->all());
+        return redirect()->route('tasks.index');
+    }
+
+    public function destroy(Task $task)
+    {
+        $task->delete();
+        return redirect()->route('tasks.index');
+    }
 }
